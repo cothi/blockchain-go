8e9f43bf66e5edd10ae3cb53146d8f5cd267ce3e
woong-s <jiungdev@gmail.com>
2022-04-26 00:08:58 +0900

    feat: chat response

--- p2p/p2p.go
+++ p2p/p2p.go
@@ -1,8 +1,10 @@
 package p2p
 
 import (
+	"fmt"
 	"net/http"
 	"tetgo/tetgocoin/utill"
+	"time"
 
 	"github.com/gorilla/websocket"
 )
@@ -10,6 +12,21 @@ import (
 var upgrader = websocket.Upgrader{}
 
 func Upgrade(rw http.ResponseWriter, r *http.Request) {
-	_, err := upgrader.Upgrade(rw, r, nil)
+	upgrader.CheckOrigin = func(r *http.Request) bool {
+		return true
+	}
+	conn, err := upgrader.Upgrade(rw, r, nil)
 	utill.HandleErr(err)
+
+	for {
+		_, p, err := conn.ReadMessage()
+		if err != nil {
+			break
+		}
+
+		fmt.Printf("Just got: %s\n\n", p)
+		time.Sleep(5 * time.Second)
+		message := fmt.Sprintf("New message: %s", p)
+		utill.HandleErr(conn.WriteMessage(websocket.TextMessage, []byte(message)))
+	}
 }
